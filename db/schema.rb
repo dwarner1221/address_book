ee9f26471447365d18bde443806cdf63e3ae517e
Donald Warner II <dwarner1221@gmail.com>
2016-08-26 01:41:17 +0000

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826012518) do
+ActiveRecord::Schema.define(version: 20160826013842) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "f_name",       null: false
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20160826012518) do
     t.string   "zip"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      null: false
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
